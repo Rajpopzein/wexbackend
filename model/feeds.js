68bd56810a643664264f868daec243dfa74780e3
Rajkumar <46551565+Rajpopzein@users.noreply.github.com>
2024-10-21 16:29:15 +0530

    Update feeds.js

--- model/feeds.js
+++ model/feeds.js
@@ -2,10 +2,6 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-import mongoose from "mongoose";
-
-const Schema = mongoose.Schema;
-
 const PostSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
   content: { type: String, required: true },
