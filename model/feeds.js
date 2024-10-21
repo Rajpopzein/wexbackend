523b38aeb32f17fbc70661a96099a8aef1ac2fb7
Rajkumar <46551565+Rajpopzein@users.noreply.github.com>
2024-10-21 16:03:33 +0530

    Update feeds.js

--- model/feeds.js
+++ model/feeds.js
@@ -2,16 +2,21 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
 const PostSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
   content: { type: String, required: true },
   visibility: {
-    type: [String],
+    type: String,
     enum: ["public", "friends", "private"],
-    default: ["friends"],
+    default: "friends",
   },
   createdAt: { type: Date, default: Date.now },
 });
+
 
 const Post = mongoose.model("Post", PostSchema);
 
